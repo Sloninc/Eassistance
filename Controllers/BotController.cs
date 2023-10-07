b7852fcca4dd3ec188c9472dc3abcd3053845c1a
Andrei Ruk <sloninc@mail.ru>
2023-10-07 16:09:59 +0300

    Добавлен BotController

---
+++ Controllers/BotController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot.Types;
+
+namespace Eassistance.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class BotController : ControllerBase
+    {
+        [HttpPost]
+        public void Post(Update update) //Сюда будут приходить апдейты
+        {
+            Console.WriteLine(update.Message.Text);
+        }
+        [HttpGet]
+        public string Get()
+        {
+            //Здесь мы пишем, что будет видно если зайти на адрес,
+            //указаную в ngrok и launchSettings
+            return "Telegram bot was started";
+        }
+    }
+}
