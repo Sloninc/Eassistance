173f6bedc5bc149e8cb87143cc862fb8977ce14e
Andrei Ruk <sloninc@mail.ru>
2023-10-08 12:03:15 +0300

    Добавлены классы StartCommand, UpdateDistributor.

---
+++ Controllers/Commands/StartCommand.cs
@@ -0,0 +1,18 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Eassistance.Controllers.Commands
+{
+    public class StartCommand: ICommand
+    {
+        public TelegramBotClient Client => Bot.GetTelegramBot();
+
+        public string Name => "/start";
+
+        public async Task Execute(Update update)
+        {
+            long chatId = update.Message.Chat.Id;
+            await Client.SendTextMessageAsync(chatId, "Привет!");
+        }
+    }
+}
---
+++ Controllers/ITelegramUpdateListener.cs
@@ -0,0 +1,9 @@
+﻿using Telegram.Bot.Types;
+
+namespace Eassistance.Controllers
+{
+    public interface ITelegramUpdateListener
+    {
+        public Task GetUpdate(Update update);
+    }
+}
