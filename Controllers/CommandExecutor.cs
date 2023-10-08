17671f18160ae9a627fb2192086d4c897b3c38a5
Andrei Ruk <sloninc@mail.ru>
2023-10-08 12:08:11 +0300

    Добавлен класс CommandExecutor.

---
+++ Controllers/CommandExecutor.cs
@@ -0,0 +1,33 @@
+﻿using Eassistance.Controllers.Commands;
+using Telegram.Bot.Types;
+
+namespace Eassistance.Controllers
+{
+    public class CommandExecutor : ITelegramUpdateListener
+    {
+        private List<ICommand> commands;
+
+        public CommandExecutor()
+        {
+            commands = new List<ICommand>();
+            {
+                new StartCommand();
+            }
+        }
+
+        public async Task GetUpdate(Update update)
+        {
+            Message msg = update.Message;
+            if (msg.Text == null)
+                return;
+
+            foreach (var command in commands)
+            {
+                if (command.Name == msg.Text)
+                {
+                    await command.Execute(update);
+                }
+            }
+        }
+    }
+}
