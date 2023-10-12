a31474751505af3c52fcda99a9ea979934156ee4
Andrei Ruk <sloninc@mail.ru>
2023-10-12 21:57:26 +0300

    Добавлена строка подключения к БД

--- Infrastructure/DataContext.cs
+++ Infrastructure/DataContext.cs
@@ -1,17 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Eassistance.Domain;
 namespace Eassistance.Infrastructure
 {
     public class DataContext : DbContext
     {
         DbConnectionOptions _dbConnection;
-        public DbSet<Co> Warehouses { get; set; }
-        public DbSet<Categories> Categories { get; set; }
-        public DbSet<Products> Products { get; set; }
-        public DbSet<ChangesProducts> ChangesProducts { get; set; }
-        public DbSet<Dishes> Dishes { get; set; }
-        public DbSet<Recipes> Recipes { get; set; }
-        public DbSet<Ingredients> Ingredients { get; set; }
-
+        public DbSet<Equipments> Equipments { get; set; }
+        public DbSet<Operations> Operations { get; set; }
+        public DbSet<Units> Units { get; set; }
+        public DbSet<Users> Users { get; set; }
         public DataContext(IConfiguration configuration)
         {
             _dbConnection = configuration.GetSection(DbConnectionOptions.KeyValue).Get<DbConnectionOptions>();
